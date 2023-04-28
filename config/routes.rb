faf37f77faedafe2ac4a1b87cd36ea37d4996141
sa7178702@gmail.com <sa7178702@gmail.com>
2023-04-29 00:58:45 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+  get 'home/contact_us'
+  # root 'home#index'
+  root to: "home#index"
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
